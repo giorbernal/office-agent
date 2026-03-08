cb63a26a9c2aa175e33d9dd3d6e90789a3564e46
Jorge Bernal Chaves <restless_project@hotmail.es>
2026-03-09 00:28:15 +0100

    adjusting docker image

--- docker/run.sh
+++ docker/run.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-if [[ $# != 1 ]]; then
-    echo "Error: params requiered!"
-    echo "Usage: ./run.sh myWorkspace (being myWorkspace a folder in this project)"
+if [[ ($# < 1) || ($# > 2) ]]; then
+    echo "Error: Error in params!"
+    echo "Usage: ./run.sh myWorkspace [port] (being myWorkspace a folder in this project)"
     exit -1
 fi
 
 WORKSPACE=$1
-echo "launching opencode for workspace $WORKSPACE ..."
+PORT=${2:-4096}
+echo "launching opencode for workspace $WORKSPACE in port $PORT ..."
 
 docker run -it \
-  -p 4096:4096 \
+  -p $PORT:4096 \
   -v "$HOME/.config/opencode:/root/.config/opencode" \
   -v "$(pwd)/$WORKSPACE:/workspace" \
   --name $WORKSPACE \
